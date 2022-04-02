090ad6feedbd52b5714cf94f525a9abfb400bbce
Bartlomiej <szybiakbartlomiej@gmail.com>
2022-04-02 12:14:39 +0200

    Fix spaces

--- src/main/java/com/sda/weather/UseInterfaces.java
+++ src/main/java/com/sda/weather/UseInterfaces.java
@@ -5,48 +5,41 @@ import java.util.Scanner;
 
 public class UseInterfaces {
 
-           public void run(){
-               System.out.println("Aplikacja jest uruchomiona\n");
-               Scanner scanner = new Scanner(System.in);
+    public void run() {
+        System.out.println("Aplikacja jest uruchomiona\n");
+        Scanner scanner = new Scanner(System.in);
 
+        while (true) {
 
-               while (true){
+            System.out.println("Witaj Serwisie Pogodowym, co chesz zrobić ?");
+            System.out.println("1. Dodać nowe dane ");
+            System.out.println("0. Zamknąć aplikacje");
 
-                   System.out.println("Witaj Serwisie Pogodowym, co chesz zrobić ?");
-                   System.out.println("1. Dodać nowe dane ");
-                   System.out.println("0. Zamknąć aplikacje");
+            int option = scanner.nextInt();
+            switch (option) {
+                case 1:
+                    createLocation();
+                    break;
+                case 0:
+                    return;
+                default:
+                    System.out.println("Nie ma takiej opcji");
+            }
+        }
+    }
 
-                   int option = scanner.nextInt();
-                   switch (option){
-                       case 1:
-                           createLocation();
-                           break;
-                       case 0:
-                           return;
-                       default:
-                           System.out.println("Nie ma takiej opcji");
-                   }
+    private void createLocation() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj nazwe miasta ");
+        String city = scanner.nextLine();
+        System.out.println("Podaj dlugosc geograficzna");
+        Double longitude = scanner.nextDouble();
+        System.out.println("Podaj szerokosc geograficzną");
+        Double latitude = scanner.nextDouble();
+        System.out.println("Podaj region");
+        String region = scanner.nextLine();
+        System.out.println("Podaj Nazwe kraju");
+        String country = scanner.nextLine();
 
-               }
-
-
-           }
-
-           private void createLocation(){
-               Scanner scanner = new Scanner(System.in);
-               System.out.println("Podaj nazwe miasta ");
-               String city = scanner.nextLine();
-               System.out.println("Podaj dlugosc geograficzna");
-               Double longitude = scanner.nextDouble();
-               System.out.println("Podaj szerokosc geograficzną");
-               Double latitude = scanner.nextDouble();
-               System.out.println("Podaj region");
-               String region = scanner.nextLine();
-               System.out.println("Podaj Nazwe kraju");
-               String country = scanner.nextLine();
-
-           }
-
-
-
+    }
 }
